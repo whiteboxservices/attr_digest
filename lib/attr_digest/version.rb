91d21275e59c29475826465b71653f4530158392
Miguel Montalvo <miguelmontalvo@MacBook-Air-de-Miguel.local>
2022-12-28 17:44:58 +0100

    Update argon2 to version 2.2.0

--- lib/attr_digest/version.rb
+++ lib/attr_digest/version.rb
@@ -2,7 +2,7 @@ module AttrDigest
   module VERSION
     MAJOR = 2
     MINOR = 1
-    TINY  = 1
+    TINY  = 2
     PRE   = nil
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
     SUMMARY = "AttrDigest v#{STRING}"
